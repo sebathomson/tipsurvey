d2b9aa07767092eee8cd049135369fdbc245edea
Sebastian Thomson <seba.thomson@gmail.com>
2013-11-08 21:57:28 -0300

    Agregados los diseños y los 2 nuevos bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Tipddy\SurveyBundle\TipddySurveyBundle(),
+            new Tipddy\SecurityBundle\TipddySecurityBundle(),
+            new Tipddy\BackendBundle\TipddyBackendBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
