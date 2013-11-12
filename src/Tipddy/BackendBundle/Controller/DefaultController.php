44bec8f66515d34ae595f2da6c6e5807299b542a
Sebastian Thomson <seba.thomson@gmail.com>
2013-11-11 21:17:54 -0300

    commit del 20131111

--- src/Tipddy/BackendBundle/Controller/DefaultController.php
+++ src/Tipddy/BackendBundle/Controller/DefaultController.php
@@ -4,10 +4,9 @@ namespace Tipddy\BackendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('TipddyBackendBundle:Default:index.html.twig', array('name' => $name));
+class DefaultController extends Controller {
+    
+    public function indexAction() {
+        return $this->render('TipddyBackendBundle:Default:index.html.twig');
     }
 }
